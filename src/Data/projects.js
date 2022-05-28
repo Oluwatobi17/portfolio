b211b669192db188e69209651b62d5e5dba28da3
Oluwatobi17 <ganiuibrahim3000@gmail.com>
2022-05-28 05:13:34 +0100

    Update projects

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,12 +1,18 @@
 const Projects = [
+    {title:'Missile Blog', link: 'https://missileblog.vercel.app/',
+        description: 'A React Blog that uses React Hooks, React Router V6, Firebase for database to perform CRUD operations'},
     {title:'Quote Generator', link: 'https://quote-generator-niu5.onrender.com/',
         description: 'A React App that Generate Quotes using a public API'},
     {title:'NotePad', link: 'https://mynotepad-5e6a7.web.app/',
-        description: 'A React App that allow you save notes'},
+        description: 'A React App that allow you to write note, read, update and delete your notes (CRUD)'},
     {title:'Portfolio', link: 'https://portfolio-f576a.web.app/',
         description: 'My Frontend Web Development Portfolio'},
     {title:'Pub', link: 'https://pub-six.vercel.app',
         description: 'Pub is a side project about beers. The project was created using NextJs and API from https://punkapi.com.'},
+    {title:'Easybank Landing Page', link: 'https://easybank-landing-page-eight-bice.vercel.app/',
+        description: 'A frontend Mentor landing page challenge using HTML5, CSS3 and Javascript'},
+    {title:'Insure Landing Page', link: 'https://insure-landingpage-wine.vercel.app/',
+        description: 'A frontend Mentor landing page challenge using HTML5, CSS3 and Javascript'},
 ];
 
 
