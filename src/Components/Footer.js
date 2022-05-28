e4b37d80cb56757b58e264dbb49696433da36afb
Oluwatobi17 <ganiuibrahim3000@gmail.com>
2022-05-28 06:04:05 +0100

    update project

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,8 @@ const Footer = () =>{
 	let date = new Date();
 	return <footer>
 		Copyright © {date.getFullYear} All rights reserved - Ganiu Ibrahim Olalekan
+		<br/>
+		<a href="https://github.com/Oluwatobi17/" target="_blank" rel="noreferrer">My GitHub</a>
 	</footer>
 }
 
