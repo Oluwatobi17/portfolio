b2cad197b06fd56c91390d8d3d458f68e6e396a6
Oluwatobi17 <ganiuibrahim3000@gmail.com>
2022-05-29 01:06:50 +0100

    iframe to Picture

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom";
-
 const Project = (props) =>{
-    const {title, link, description} = props.project;
+    const {title, link, description, img} = props.project;
 
     return <div className="project-container">
-        <iframe src={link} width="555" height="200" title={title}>
+        {/* <iframe src={link} width="555" height="200" title={title}>
             Sorry your browser does not support inline frames.
-        </iframe>
+        </iframe> */}
+        <img src={img} alt="project preview"/>
         <h2>{title}</h2>
 
         <p>{description}</p>
 
-        <a href={link} target='_blank'>Visit</a>
+        <a href={link} target='_blank' rel='noreferrer'>Visit</a>
     </div>
 }
 
