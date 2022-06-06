6d6ae7721d8cbd55b74942c6d65d6e12e01eb1b3
Oluwatobi17 <ganiuibrahim3000@gmail.com>
2022-06-06 04:34:40 +0100

    About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -23,7 +23,7 @@ const About = () =>{
 
             <h1 className="title2">My Skills?</h1>
             <p className="info">
-                I have about 2 years of experience with frontend web development and its 
+                I have about 3 years of experience with frontend web development and its 
                  technologies such as HTML5, CSS3, JavaScript, jQuery, React, Redux,
                   GitHub, Adobe Photoshop, NextJs and MQL4
                 I can provide clean code for a responsive web page accessible to all users 
